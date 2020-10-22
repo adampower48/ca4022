6060fd3eef2e17643fafd871553bd4a7b2247a8c
adampower48 <adam.power48@mail.dcu.ie>
2020-10-22 12:46:08 +0100

    Move hive setup commands.

--- setup_scripts/hive_setup.sh
+++
@@ -1,5 +0,0 @@
-# Make folders on dfs
-hdfs dfs -mkdir /tmp
-hdfs dfs -mkdir -p /user/hive/warehouse
-hdfs dfs -chmod g+w /tmp
-hdfs dfs -chmod g+w /user/hive/warehouse
--- setup_scripts/start_hive.sh
+++ setup_scripts/start_hive.sh
@@ -1,1 +1,7 @@
+# Make folders on dfs
+hdfs dfs -mkdir /tmp
+hdfs dfs -mkdir -p /user/hive/warehouse
+hdfs dfs -chmod g+w /tmp
+hdfs dfs -chmod g+w /user/hive/warehouse
+
 ${HADOOP_HOME}/sbin/start-yarn.sh
